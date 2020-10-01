3d15321bcce898756580e955da22d4ec94432f56
flxbrtz <72187030+flxbrtz@users.noreply.github.com>
2020-10-01 09:50:16 +0200

    Update Student.java

--- src/Verwaltung/Verwaltung/Student.java
+++ src/Verwaltung/Verwaltung/Student.java
@@ -34,36 +34,43 @@ public class Student
 	
 	
 	// Getter
+	/**gets the first name of the student*/
 	public String GetVorname() 
 	{
 		return Vorname;
 	}
 	
+	/**gets the family name of the student*/
 	public String GetName() 
 	{
 		return Name;
 	}
 	
+	/**gets the email adress of the student*/
 	public String GetEmail() 
 	{
 		return Email;
 	}
 	
+	/**gets the grade of the student*/
 	public String GetKlassenStufe() 
 	{
 		return KlassenStufe;
 	}
 	
+	/**gets the name of the students class*/
 	public String GetKlassenName() 
 	{
 		return KlassenName;
 	}
 	
+	/**gets the students date of birth*/
 	public Date GetGeburtsdatum() 
 	{
 		return Geburtsdatum;
 	}
 	
+	/**gets the students gender*/
 	public String GetGeschlecht() 
 	{
 		return Geschlecht;
@@ -71,36 +78,43 @@ public class Student
 	// Ende Getter
 	
 	// Setter
+	/**sets the first name of the student*/
 	public void SetVorname(String NewVorname) 
 	{
 		this.Vorname = NewVorname;
 	}
 	
+	/**sets the family name of the student*/
 	public void SetName(String newName) 
 	{
 		this.Name = newName;
 	}
 	
+	/**sets the email adress of the student*/
 	public void SetEmail(String NewEmail) 
 	{
 		this.Email = NewEmail;
 	}
 	
+	/**sets the grade of the student*/
 	public void SetKlassenStufe(String NewKlassenStufe) 
 	{
 		this.KlassenStufe = NewKlassenStufe;
 	}
 	
+	/**sets the name of the students class*/
 	public void SetKlassenName(String NewKlassenName) 
 	{
 		this.KlassenName = NewKlassenName;
 	}
 	
+	/**sets the students date of birth*/
 	public void SetGeburtsdatum(Date NewDate) 
 	{
 		this.Geburtsdatum =  NewDate;
 	}
 	
+	/**sets the students gender*/
 	public void SetGeschlecht(String NewGeschlecht) 
 	{
 		this.Geschlecht = NewGeschlecht;
